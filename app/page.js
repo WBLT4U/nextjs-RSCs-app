7e9a2ed48c9a2d67db42f8f3d3d7883523f4bf89
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2025-08-10 14:15:00 +0100

    adding dynamic functions in page file

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import clientPromise from '@/lib/mongodb';
 import { Suspense } from 'react';
 
@@ -6,13 +8,11 @@ import UsePromiseDemo from '@/components/UsePromisesDemo';
 import ErrorBoundary from '@/components/ErrorBoundary';
 
 export default async function Home() {
-  // Directly fetch from MongoDB
   const fetchUsersPromise = (async () => {
     const client = await clientPromise;
     const db = client.db("rsc_demo");
     const users = await db.collection("users").find().sort({ createdAt: -1 }).toArray();
 
-    // Convert ObjectId to string
     return users.map(user => ({
       ...user,
       _id: user._id.toString()
